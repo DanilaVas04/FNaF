909a2f0e5ccb7bdfd362dfa20cafcc69e0ea2a3e
DanilaVas04 <danyavas04@mail.ru>
2025-02-23 18:06:05 +0300

    Initial commit

---
+++ Assets/Материалы/Button/Switch_Door.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Switch_Door : MonoBehaviour
+{
+    private bool isPressed = false;
+    public Material Red_Material;
+    public Material Green_Material;
+    public GameObject objectToMove;
+    public Vector3 targetPositionUp;
+    public Vector3 targetPositionDown;
+    public float speed = 1.0f;
+    public AudioSource SoundDoor;
+
+    void Start()
+    {
+        ChangeObjectMaterial(Red_Material);
+    }
+
+    void OnMouseDown()
+    {
+        if (isPressed)
+        {
+            isPressed = false;
+            ChangeObjectMaterial(Red_Material);
+            SoundDoor.Play();
+        }
+        else
+        {
+            isPressed = true;
+            ChangeObjectMaterial(Green_Material);
+            SoundDoor.Play();
+        }
+    }
+
+    void ChangeObjectMaterial(Material material)
+    {
+        // Получаем компонент Renderer объекта
+        Renderer renderer = GetComponent<Renderer>();
+
+        // Проверяем, что компонент Renderer существует
+        if (renderer != null)
+        {
+            // Меняем материал объекта
+            renderer.material = material;
+        }
+    }
+
+    void Update()
+    {
+        if (!isPressed)
+        {
+            if (objectToMove != null)
+            {
+                objectToMove.transform.position = Vector3.Lerp(objectToMove.transform.position, targetPositionUp, speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (objectToMove != null)
+            {
+                objectToMove.transform.position = Vector3.Lerp(objectToMove.transform.position, targetPositionDown, speed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Материалы/Button/Switch_Light.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+public class Switch_Light : MonoBehaviour
+{
+    private bool isPressed = false;
+    public Material Gray_Material;
+    public Material Light_Material;
+    public Light spotLight;
+    public float minFlickerInterval = 0.1f; // Минимальный интервал между морганиями
+    public float maxFlickerInterval = 0.5f; // Максимальный интервал между морганиями
+    private float timer;
+    private float flickerInterval;
+    public AudioSource SoundLight;
+
+    void Start()
+    {
+        ChangeObjectMaterial(Gray_Material);
+        TurnOffLight();
+    }
+
+    void OnMouseDown()
+    {
+        if (isPressed)
+        {
+            isPressed = false;
+            ChangeObjectMaterial(Gray_Material);
+            TurnOffLight();
+            SoundLight.Stop();
+        }
+        else
+        {
+            isPressed = true;
+            ChangeObjectMaterial(Light_Material);
+            TurnOnLight();
+            SoundLight.Play();
+        }
+    }
+
+    void ChangeObjectMaterial(Material material)
+    {
+        // Получаем компонент Renderer объекта
+        Renderer renderer = GetComponent<Renderer>();
+
+        // Проверяем, что компонент Renderer существует
+        if (renderer != null)
+        {
+            // Меняем материал объекта
+            renderer.material = material;
+        }
+    }
+
+    void TurnOnLight()
+    {
+        if (spotLight != null)
+        {
+            spotLight.enabled = true;
+        }
+    }
+
+    void TurnOffLight()
+    {
+        if (spotLight != null)
+        {
+            spotLight.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            // Уменьшаем таймер
+            timer -= Time.deltaTime;
+
+            // Если таймер истек, переключаем свет
+            if (timer <= 0)
+            {
+                ToggleLight();
+                SetRandomFlickerInterval();
+            }
+        }
+    }
+    void ToggleLight()
+    {
+        if (spotLight != null)
+        {
+            spotLight.enabled = !spotLight.enabled;
+        }
+    }
+
+    void SetRandomFlickerInterval()
+    {
+        // Устанавливаем случайный интервал для следующего моргания
+        flickerInterval = Random.Range(minFlickerInterval, maxFlickerInterval);
+        timer = flickerInterval;
+    }
+}
