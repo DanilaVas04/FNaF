c8a5a39793ab00714f046910789c7d72056b4445
Dmitry OLeD <dm.bashkirov@gmail.com>
2025-04-17 00:26:22 +0300

    1_5_5
    Added JoyStick

--- Assets/MATERIALS/Mini First Person Controller/Scripts/Components/Jump.cs
+++ Assets/MATERIALS/Mini First Person Controller/Scripts/Components/Jump.cs
@@ -33,13 +33,5 @@ public class Jump : MonoBehaviour
     }
 
 
-    public void JumpButton()
-    {
-        // Jump when the Jump button is pressed and we are on the ground.
-        if ((!groundCheck || groundCheck.isGrounded))
-        {
-            rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
-            Jumped?.Invoke();
-        }
-    }
+   
 }
